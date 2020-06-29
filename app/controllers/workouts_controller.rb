9c2771c2d896ec8f97718407fb5d75cb765d0156
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-29 18:03:16 +0000

    more delete fixes

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,50 +2,55 @@ class WorkoutsController < ApplicationController
 
   #create
   
-  get "/workouts/new.html" do
-    erb :"/workouts/new.html"
-  end
-  
-  post "/workouts" do
-    @workout = Workout.create(
-      name: params[:name], 
-      date: params[:date]
-      )
-     redirect "/workouts/#{@workout.id}" 
-  end
+    get "/workouts/new.html" do
+      erb :"/workouts/new.html"
+    end
+    
+    post "/workouts" do
+      @workout = Workout.create(
+        name: params[:name], 
+        date: params[:date]
+        )
+       redirect "/workouts/#{@workout.id}" 
+    end
 
   #read
   
-  get "/workouts/:id" do
-    @workout = Workout.find(params[:id])
-    erb :"/workouts/show.html"
-  end
-
-  get "/workouts" do
-    @workouts = Workout.all
-    erb :"/workouts/index.html"
-  end
+    get "/workouts/:id" do
+      @workout = Workout.find(params[:id])
+      erb :"/workouts/show.html"
+    end
+  
+    get "/workouts" do
+      @workouts = Workout.all
+      erb :"/workouts/index.html"
+    end
   
   #update
   
-  get "/workouts/:id/edit" do
-    @workout = Workout.find(params[:id])  
-    erb :"/workouts/edit.html"
-  end
-  
-  post "/workouts/:id" do
-    @workout = Workout.find(params[:id])
-    @workout.update(
-      name: params[:name], 
-      date: params[:date]
-      )
-    redirect "/workouts/#{@workout.id}"
-  end
+    get "/workouts/:id/edit" do
+      @workout = Workout.find(params[:id])  
+      erb :"/workouts/edit.html"
+    end
+    
+    post "/workouts/:id" do
+      @workout = Workout.find(params[:id])
+      @workout.update(
+        name: params[:name], 
+        date: params[:date]
+        )
+      redirect "/workouts/#{@workout.id}"
+    end
 
   #delete
-  delete "/workouts/:id/delete" do
-    @workout = Workout.find(params[:id])
-    @order.delete
-    redirect "/workouts"
-  end
+  
+    delete "/workouts/:id/delete" do
+      @workout = Workout.find(params[:id])
+      @order.destroy
+      redirect "/workouts"
+    end
+    
+    post  "/workouts/:id/delete" do
+      redirect "/workouts"
+    end
 end
