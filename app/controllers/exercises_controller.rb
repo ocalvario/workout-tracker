8ffb0ce11c9a81dd285c2d3ef36b05728f0e7be9
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-29 01:18:00 +0000

    fixes titles

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,37 +1,37 @@
-class ExercisEsController < ApplicationController
+class ExercisesController < ApplicationController
 
-  # GET: /exercis_es
-  get "/exercis_es" do
-    erb :"/exercis_es/index.html"
+  # GET: /exercises
+  get "/exercises" do
+    erb :"/exercises/index.html"
   end
 
-  # GET: /exercis_es/new
-  get "/exercis_es/new" do
-    erb :"/exercis_es/new.html"
+  # GET: /exercises/new
+  get "/exercises/new" do
+    erb :"/exercises/new.html"
   end
 
-  # POST: /exercis_es
-  post "/exercis_es" do
-    redirect "/exercis_es"
+  # POST: /exercises
+  post "/exercises" do
+    redirect "/exercises"
   end
 
-  # GET: /exercis_es/5
-  get "/exercis_es/:id" do
-    erb :"/exercis_es/show.html"
+  # GET: /exercises/5
+  get "/exercises/:id" do
+    erb :"/exercises/show.html"
   end
 
-  # GET: /exercis_es/5/edit
-  get "/exercis_es/:id/edit" do
-    erb :"/exercis_es/edit.html"
+  # GET: /exercises/5/edit
+  get "/exercises/:id/edit" do
+    erb :"/exercises/edit.html"
   end
 
-  # PATCH: /exercis_es/5
-  patch "/exercis_es/:id" do
-    redirect "/exercis_es/:id"
+  # PATCH: /exercises/5
+  patch "/exercises/:id" do
+    redirect "/exercises/:id"
   end
 
-  # DELETE: /exercis_es/5/delete
-  delete "/exercis_es/:id/delete" do
-    redirect "/exercis_es"
+  # DELETE: /exercises/5/delete
+  delete "/exercises/:id/delete" do
+    redirect "/exercises"
   end
 end
