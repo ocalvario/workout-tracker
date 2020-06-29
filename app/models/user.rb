0271c6f19a24eba1e7a9736ef363559399ced52a
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-29 04:07:31 +0000

    conventions for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  has_many :workout
+  has_many :workouts
   has_secure_password 
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,4 @@
 class Workout < ActiveRecord::Base
-  has_many :exercise
+  has_many :exercises
   belongs_to :user
 end
