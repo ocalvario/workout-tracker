cf931262318916ab80ed7a51c5264cf1ad77e458
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-29 03:08:47 +0000

    add new workout

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -6,32 +6,37 @@ class WorkoutsController < ApplicationController
   end
 
   # GET: /workou_ts/new
-  get "/workouts/new" do
+  get "/workouts/new.html" do
     erb :"/workouts/new.html"
   end
 
-  # POST: /workouts
-  post "/workouts" do
-    redirect "/workouts"
+  # GET: /workouts/new
+  post "/workouts/new" do
+    erb :"/workouts/index.html"
   end
 
-  # GET: /workouts/5
-  get "/workouts/:id" do
-    erb :"/workouts/show.html"
-  end
+#   # POST: /workouts
+#   post "/workouts" do
+#     redirect "/workouts"
+#   end
 
-  # GET: /workou_ts/5/edit
-  get "/workouts/:id/edit" do
-    erb :"/workouts/edit.html"
-  end
+#   # GET: /workouts/5
+#   get "/workouts/:id" do
+#     erb :"/workouts/show.html"
+#   end
 
-  # PATCH: /workouts/5
-  patch "/workouts/:id" do
-    redirect "/workouts/:id"
-  end
+#   # GET: /workou_ts/5/edit
+#   get "/workouts/:id/edit" do
+#     erb :"/workouts/edit.html"
+#   end
 
-  # DELETE: /workouts/5/delete
-  delete "/workouts/:id/delete" do
-    redirect "/workouts"
-  end
+#   # PATCH: /workouts/5
+#   patch "/workouts/:id" do
+#     redirect "/workouts/:id"
+#   end
+
+#   # DELETE: /workouts/5/delete
+#   delete "/workouts/:id/delete" do
+#     redirect "/workouts"
+#   end
 end
